85c3b3dfb37094a6e647ea8d1d617284d22eb815
Dahoom <Rozimo56@gmail.com>
2023-08-05 08:12:55 +0300

    update script

--- script.py
+++ script.py
@@ -8,4 +8,8 @@ def read_and_extract(file_path):
 def write_to_sql(df, output_file):
     with open(output_file, 'w') as f:
         for _, row in df.iterrows():
-            f.write(f"INSERT INTO your_table (network, country_code, state, city) VALUES ('{row['network']}', '{row['country_code']}', '{row['state']}', '{row['city']}');\n")
+            f.write(f"INSERT INTO ipv4 (network, country_code, state, city) VALUES ('{row['network']}', '{row['country_code']}', '{row['state']}', '{row['city']}');\n")
+
+if __name__ == "__main__":
+    df = read_and_extract('geolocationDatabaseIPv4.csv')
+    write_to_sql(df, 'db.sql')
