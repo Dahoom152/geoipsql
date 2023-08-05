4228a8a10d77abac69de384c3f65a151164461ce
Dahoom <Rozimo56@gmail.com>
2023-08-05 08:26:51 +0300

    update

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import gzip
 
 def read_and_extract(file_path):
     df = pd.read_csv(file_path)
@@ -6,10 +7,10 @@ def read_and_extract(file_path):
     return df
 
 def write_to_sql(df, output_file):
-    with open(output_file, 'w') as f:
+    with gzip.open(output_file, 'wt') as f:
         for _, row in df.iterrows():
             f.write(f"INSERT INTO ipv4 (network, country_code, state, city) VALUES ('{row['network']}', '{row['country_code']}', '{row['state']}', '{row['city']}');\n")
 
 if __name__ == "__main__":
     df = read_and_extract('geolocationDatabaseIPv4.csv')
-    write_to_sql(df, 'db.sql')
+    write_to_sql(df, 'db.sql.gz')
